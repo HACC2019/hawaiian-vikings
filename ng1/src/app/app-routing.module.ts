a11bcca0b6e9bc6d1e25923d19d1444ec0fc50dc
Jung No <hirejungno@gmail.com>
2019-11-11 13:44:23 -1000

    adding chat1 line chart

--- ng1/src/app/app-routing.module.ts
+++ ng1/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: "", component: HomeComponent},
+  { path: "", component: MyLineChartComponent},
+  { path: "chart1", component: MyLineChartComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng1/src/app/app.module.ts
+++ ng1/src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
+import{ ChartsModule} from 'ng2-charts';
+import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MyLineChartComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    ChartsModule
   ],
   exports: [
   ],
